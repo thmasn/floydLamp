b543ebbfe15658b741f7bac2a1fbe05f7d7532f5
thmasn <thomaspahler@t-online.de>
2019-01-13 00:07:01 +0100

    Add files via upload
    webapp

---
+++ sw.js
@@ -0,0 +1,14 @@
+importScripts('sw-toolbox.js');
+
+toolbox.precache([
+  'companion.js',
+  'index.html',
+  'floyd.css',
+  'images/floydlampApp4-11.png',
+  'images/floydlampApp4-12.png',
+  'images/floydlampApp4-13.png',
+  'images/floydlampApp4-14.png',
+  'images/floydlampApp4-15.png',
+  'images/floydlampApp4-16.png',
+  'images/floydlampApp4-17.png',
+]);
