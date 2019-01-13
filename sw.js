fa51494a9e56a43fe74a02269f0fcfd2dbb30439
thmasn <thomaspahler@t-online.de>
2019-01-13 13:13:32 +0100

    simplified triangle gradient, ui changes

--- sw.js
+++ sw.js
@@ -5,10 +5,9 @@ toolbox.precache([
   'index.html',
   'floyd.css',
   'images/floydlampApp4-11.png',
-  'images/floydlampApp4-12.png',
+  'images/floydlampApp4-22.png',
   'images/floydlampApp4-13.png',
   'images/floydlampApp4-14.png',
-  'images/floydlampApp4-15.png',
-  'images/floydlampApp4-16.png',
-  'images/floydlampApp4-17.png',
+  'images/floydlampApp4-21.svg',
+  'images/color_wheel.png',
 ]);
